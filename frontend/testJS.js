82e26b07c73a7ea567c86fa0796f3df824461b94
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-17 12:15:20 +0200

    the JS-code can be executed in java (vaadin) with the comment below

---
+++ frontend/testJS.js
@@ -0,0 +1,15 @@
+window.com = {
+  testJ : function() {
+    return "creating identity";
+  }
+}
+
+//    submit.addClickListener(
+//        event -> {
+//          UI.getCurrent()
+//              .getPage()
+//              .executeJs("return com.testJ()")
+//              .then(r -> System.out.println(r.asString()));
+//        }
+//
+//    );
