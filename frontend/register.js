c2ce7b5565a6e6f7b1b01158fef7264008c79e70
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-17 12:16:40 +0200

    The code I want to execute in java

---
+++ frontend/register.js
@@ -0,0 +1,28 @@
+const {AccountBuilder, ExplorerUrl} = require('@iota/identity-wasm/node')
+
+window.register = {
+
+  createDid : async function (){
+    console.log("creating identity... This might take some time to complete POW")
+
+    // The creation step generates a keypair, builds an identity
+    // and publishes it to the IOTA mainnet.
+    let builder = new AccountBuilder();
+    let account = await builder.createIdentity();
+
+    // Retrieve the DID of the newly created identity.
+    let did = account.did();
+
+    // Print the DID of the created Identity.
+    console.log(did.toString())
+
+    // Print the local state of the DID Document
+    console.log(account.document());
+
+    // Print the Explorer URL for the DID.
+    console.log(`Explorer Url:`, ExplorerUrl.mainnet().resolverUrl(did));
+
+    return account.document();
+  }
+
+}
