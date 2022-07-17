1ec3f2f48b384c0c4bc227c6bc60bc6f572904bf
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-17 12:12:53 +0200

    initial commit

---
+++ src/main/java/com/example/iota2/Iota2Application.java
@@ -0,0 +1,13 @@
+package com.example.iota2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iota2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Iota2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/iota2/Iota2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iota2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iota2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
